8cc21e4c45eafe25241b2fcfe00c09aa26f39e51
coffeegoddd☕️✨ <dustin@dolthub.com>
2025-07-28 11:09:24 -0700

    /mcp/pkg/tools: wip, adding procedures

---
+++ mcp/pkg/tools/create_dolt_branch.go
@@ -0,0 +1,96 @@
+package tools
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/dolthub/dolt-mcp/mcp/pkg"
+	"github.com/dolthub/dolt-mcp/mcp/pkg/db"
+	"github.com/mark3labs/mcp-go/mcp"
+)
+
+const (
+	CreateDoltBranchToolName                              = "create_dolt_branch"
+	CreateDoltBranchToolOriginalBranchArgumentDescription = "The name of the branch to copy."
+	CreateDoltBranchToolNewBranchArgumentDescription      = "The name of the new branch."
+	CreateDoltBranchToolForceArgumentDescription          = "If true, will force the creation of the new branch even if it already exists."
+	CreateDoltBranchToolDescription                       = "Creates a new branch from the specified original branch."
+	CreateDoltBranchToolCallSuccessFormatString           = "successfully created branch: %s"
+	CreateDoltBranchToolSQLQueryFormatString              = "CALL DOLT_BRANCH('-c', '%s', '%s');"
+	CreateDoltBranchToolForceSQLQueryFormatString         = "CALL DOLT_BRANCH('-f', '-c', '%s', '%s');"
+)
+
+func RegisterCallDoltBranchTool(server pkg.Server) {
+	mcpServer := server.MCP()
+
+	createDoltBranchTool := mcp.NewTool(
+		CreateDoltBranchToolName,
+		mcp.WithDescription(CreateDoltBranchToolDescription),
+		mcp.WithString(
+			OriginalBranchCallToolArgumentName,
+			mcp.Required(),
+			mcp.Description(CreateDoltBranchToolOriginalBranchArgumentDescription),
+		),
+		mcp.WithString(
+			NewBranchCallToolArgumentName,
+			mcp.Required(),
+			mcp.Description(CreateDoltBranchToolNewBranchArgumentDescription),
+		),
+		mcp.WithBoolean(
+			ForceCallToolArgumentName,
+			mcp.Description(CreateDoltBranchToolForceArgumentDescription),
+		),
+	)
+
+	mcpServer.AddTool(createDoltBranchTool, func(ctx context.Context, request mcp.CallToolRequest) (result *mcp.CallToolResult, serverErr error) {
+		var err error
+		var originalBranch string
+		originalBranch, err = GetRequiredStringArgumentFromCallToolRequest(request, OriginalBranchCallToolArgumentName)
+		if err != nil {
+			result = mcp.NewToolResultError(err.Error())
+			return
+		}
+
+		var newBranch string
+		newBranch, err = GetRequiredStringArgumentFromCallToolRequest(request, NewBranchCallToolArgumentName)
+		if err != nil {
+			result = mcp.NewToolResultError(err.Error())
+			return
+		}
+
+		force := GetBooleanArgumentFromCallToolRequest(request, ForceCallToolArgumentName)
+
+		config := server.DBConfig()
+		var tx db.DatabaseTransaction
+		tx, err = db.NewDatabaseTransaction(ctx, config)
+		if err != nil {
+			result = mcp.NewToolResultError(err.Error())
+			return
+		}
+
+		defer func() {
+			rerr := CommitTransactionOrRollbackOnError(ctx, tx, err)
+			if rerr != nil {
+				result = mcp.NewToolResultError(rerr.Error())
+			}
+		}()
+
+		if force {
+			err = tx.ExecContext(ctx, fmt.Sprintf(CreateDoltBranchToolForceSQLQueryFormatString, originalBranch, newBranch))
+			if err != nil {
+				result = mcp.NewToolResultError(err.Error())
+				return
+			}
+		} else {
+			err = tx.ExecContext(ctx, fmt.Sprintf(CreateDoltBranchToolSQLQueryFormatString, originalBranch, newBranch))
+			if err != nil {
+				result = mcp.NewToolResultError(err.Error())
+				return
+			}
+		}
+
+		result = mcp.NewToolResultText(fmt.Sprintf(CreateDoltBranchToolCallSuccessFormatString, newBranch))
+		return
+	})
+}
+
---
+++ mcp/pkg/tools/create_dolt_branch_from_head.go
@@ -0,0 +1,83 @@
+package tools
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/dolthub/dolt-mcp/mcp/pkg"
+	"github.com/dolthub/dolt-mcp/mcp/pkg/db"
+	"github.com/mark3labs/mcp-go/mcp"
+)
+
+const (
+	CreateDoltBranchFromHeadToolName                         = "create_dolt_branch_from_head"
+	CreateDoltBranchFromHeadToolNewBranchArgumentDescription = "The name of the new branch."
+	CreateDoltBranchFromHeadToolForceArgumentDescription     = "If true, will force the creation of the new branch even if it already exists."
+	CreateDoltBranchFromHeadToolDescription                  = "Creates a new branch from current HEAD."
+	CreateDoltBranchFromHeadToolCallSuccessFormatString      = "successfully created branch: %s"
+	CreateDoltBranchFromHeadToolSQLQueryFormatString             = "CALL DOLT_BRANCH('%s');"
+	CreateDoltBranchFromHeadToolForceSQLQueryFormatString        = "CALL DOLT_BRANCH('-f', '%s');"
+)
+
+func RegisterCallDoltBranchFromHeadTool(server pkg.Server) {
+	mcpServer := server.MCP()
+
+	createDoltBranchFromHeadTool := mcp.NewTool(
+		CreateDoltBranchFromHeadToolName,
+		mcp.WithDescription(CreateDoltBranchFromHeadToolDescription),
+		mcp.WithString(
+			NewBranchCallToolArgumentName,
+			mcp.Required(),
+			mcp.Description(CreateDoltBranchToolNewBranchArgumentDescription),
+		),
+		mcp.WithBoolean(
+			ForceCallToolArgumentName,
+			mcp.Description(CreateDoltBranchFromHeadToolForceArgumentDescription),
+		),
+	)
+
+	mcpServer.AddTool(createDoltBranchFromHeadTool, func(ctx context.Context, request mcp.CallToolRequest) (result *mcp.CallToolResult, serverErr error) {
+		var err error
+		var newBranch string
+		newBranch, err = GetRequiredStringArgumentFromCallToolRequest(request, NewBranchCallToolArgumentName)
+		if err != nil {
+			result = mcp.NewToolResultError(err.Error())
+			return
+		}
+
+		force := GetBooleanArgumentFromCallToolRequest(request, ForceCallToolArgumentName)
+
+		config := server.DBConfig()
+		var tx db.DatabaseTransaction
+		tx, err = db.NewDatabaseTransaction(ctx, config)
+		if err != nil {
+			result = mcp.NewToolResultError(err.Error())
+			return
+		}
+
+		defer func() {
+			rerr := CommitTransactionOrRollbackOnError(ctx, tx, err)
+			if rerr != nil {
+				result = mcp.NewToolResultError(rerr.Error())
+			}
+		}()
+
+		if force {
+			err = tx.ExecContext(ctx, fmt.Sprintf(CreateDoltBranchFromHeadToolForceSQLQueryFormatString, newBranch))
+			if err != nil {
+				result = mcp.NewToolResultError(err.Error())
+				return
+			}
+		} else {
+			err = tx.ExecContext(ctx, fmt.Sprintf(CreateDoltBranchFromHeadToolSQLQueryFormatString, newBranch))
+			if err != nil {
+				result = mcp.NewToolResultError(err.Error())
+				return
+			}
+		}
+
+		result = mcp.NewToolResultText(fmt.Sprintf(CreateDoltBranchFromHeadToolCallSuccessFormatString, newBranch))
+		return
+	})
+}
+
