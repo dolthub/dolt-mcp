9e47967302ab97e5684b7c659afa81bf63559e6e
coffeegoddd☕️✨ <dustin@dolthub.com>
2025-08-20 16:43:45 -0700

    /mcp/pkg/db/config.go: fix no database name, need a test

--- mcp/pkg/db/config.go
+++ mcp/pkg/db/config.go
@@ -45,9 +45,9 @@ func (c *Config) GetDSN() string {
 		return c.DSN
 	}
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)", c.User, c.Password, c.Host, c.Port)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/", c.User, c.Password, c.Host, c.Port)
 	if c.DatabaseName != "" {
-		dsn += fmt.Sprintf("/%s", c.DatabaseName)
+		dsn += c.DatabaseName
 	}
 
 	dsn += c.getDSNOptions()
